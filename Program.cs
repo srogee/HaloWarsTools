9df1e40beca213f5bea50ea09e576c9eba08133b
srogee <joe.sorgea@gmail.com>
2021-10-24 15:49:18 -0500

    Minor fixes

--- Program.cs
+++ Program.cs
@@ -11,14 +11,23 @@ namespace HaloWarsTools
             string outputDirectory = "C:\\Users\\rid3r\\Desktop\\HaloWarsTools";
 
             var xtt = HWXttResource.FromFile(Path.Combine(inputDirectory, "scenario\\skirmish\\design\\blood_gulch\\blood_gulch.xtt"));
+            xtt.AlbedoTexture.Save(Path.Combine(outputDirectory, "blood_gulch_albedo.png"), ImageFormat.Png);
+
             var xtd = HWXtdResource.FromFile(Path.Combine(inputDirectory, "scenario\\skirmish\\design\\blood_gulch\\blood_gulch.xtd"));
+            xtd.AmbientOcclusionTexture.Save(Path.Combine(outputDirectory, "blood_gulch_ao.png"), ImageFormat.Png);
+            xtd.OpacityTexture.Save(Path.Combine(outputDirectory, "blood_gulch_opacity.png"), ImageFormat.Png);
+            xtd.Mesh.Export(Path.Combine(outputDirectory, "blood_gulch_vismesh.obj"), GenericMeshExportFormat.Obj);
+
+            var ugx = HWUgxResource.FromFile(Path.Combine(inputDirectory, "art\\covenant\\building\\builder_01\\builder_hologram_01.ugx"));
+            ugx.Mesh.Export(Path.Combine(outputDirectory, "builder_hologram_01.obj"), GenericMeshExportFormat.Obj);
+
             var gls = HWGlsResource.FromFile(Path.Combine(inputDirectory, "scenario\\skirmish\\design\\blood_gulch\\blood_gulch.gls"));
-
-            xtt.AlbedoTexture.Save(Path.Combine(outputDirectory, "albedo.png"), ImageFormat.Png);
-            xtd.AmbientOcclusionTexture.Save(Path.Combine(outputDirectory, "ao.png"), ImageFormat.Png);
-            xtd.OpacityTexture.Save(Path.Combine(outputDirectory, "opacity.png"), ImageFormat.Png);
-
             Console.WriteLine(gls.SunColor);
+
+            var scn = HWScnResource.FromFile(Path.Combine(inputDirectory, "scenario\\skirmish\\design\\blood_gulch\\blood_gulch.scn"));
+            var sc2 = HWSc2Resource.FromFile(Path.Combine(inputDirectory, "scenario\\skirmish\\design\\blood_gulch\\blood_gulch.sc2"));
+            var sc3 = HWSc3Resource.FromFile(Path.Combine(inputDirectory, "scenario\\skirmish\\design\\blood_gulch\\blood_gulch.sc3"));
+            var vis = HWVisResource.FromFile(Path.Combine(inputDirectory, "art\\covenant\\building\\builder_03\\builder_03.vis"));
         }
     }
 }
--- Resources/HWXtdResource.cs
+++ Resources/HWXtdResource.cs
@@ -2,11 +2,14 @@
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Drawing.Imaging;
+using System.Numerics;
 
 namespace HaloWarsTools
 {
     public class HWXtdResource : HWBinaryResource
     {
+        public GenericMesh Mesh => ValueCache.Get(ImportMesh);
+
         public HWXtdResource(string filename) : base(filename) { }
 
         public Bitmap AmbientOcclusionTexture => ValueCache.Get(() => ExtractEmbeddedDXT5A(GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_AOChunk)));
@@ -78,6 +81,103 @@ namespace HaloWarsTools
             finalBitmap.RotateFlip(RotateFlipType.Rotate90FlipX); // Matches albedo texture
 
             return finalBitmap;
+        }
+
+        private GenericMesh ImportMesh() {
+            int stride = 1;
+            MeshNormalExportMode shadingMode = MeshNormalExportMode.CalculateNormalsSmoothShaded;
+
+            HWBinaryResourceChunk headerChunk = GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_XTDHeader);
+            float tileScale = BinaryUtils.ReadFloatBigEndian(RawBytes, (int)headerChunk.Offset + 12);
+            HWBinaryResourceChunk atlasChunk = GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_AtlasChunk);
+
+            int gridSize = (int)Math.Round(Math.Sqrt((atlasChunk.Size - 32) / 8)); // Subtract the min/range vector sizes, divide by position + normal size, and sqrt for grid size
+            int positionOffset = (int)atlasChunk.Offset + 32;
+            int normalOffset = positionOffset + gridSize * gridSize * 4;
+
+            if (gridSize % stride != 0) {
+                throw new Exception($"Grid size {gridSize} is not evenly divisible by stride {stride} - choose a different stride value.");
+            }
+
+            GenericMesh mesh = new GenericMesh(new MeshExportOptions(Matrix4x4.Identity, shadingMode, false, false));
+            GenericMaterial material = new GenericMaterial("mat");
+            GenericMeshSection section = new GenericMeshSection("terrain");
+
+            // These are stored as ZYX, 4 bytes per component
+            Vector3 PosCompMin = BinaryUtils.ReadVector3BigEndian(RawBytes, (int)atlasChunk.Offset).ReverseComponents();
+            Vector3 PosCompRange = BinaryUtils.ReadVector3BigEndian(RawBytes, (int)atlasChunk.Offset + 16).ReverseComponents();
+
+            // Read vertex offsets/normals and add them to the mesh
+            for (int x = 0; x < gridSize; x += stride) {
+                for (int z = 0; z < gridSize; z += stride) {
+                    int index = ConvertGridPositionToIndex(new Tuple<int, int>(x, z), gridSize);
+                    int offset = index * 4;
+
+                    // Get offset position and normal for this vertex
+                    Vector3 position = ReadVector3Compressed(positionOffset + offset) * PosCompRange - PosCompMin;
+
+                    // Positions are relative to the terrain grid, so shift them by the grid position
+                    position += new Vector3(x, 0, z) * tileScale;
+                    mesh.Vertices.Add(ConvertToUE4PositionVectorTerrain(position));
+
+                    Vector3 normal = ConvertToUE4DirectionVector(Vector3.Normalize(ReadVector3Compressed(normalOffset + offset) * 2.0f - Vector3.One));
+                    mesh.Normals.Add(normal);
+
+                    // Simple UV based on original, non-warped terrain grid
+                    Vector3 texCoord = new Vector3(x / ((float)gridSize - 1), 1 - (z / ((float)gridSize - 1)), 0);
+                    mesh.TexCoords.Add(texCoord);
+                }
+            }
+
+            // Generate faces based on terrain grid
+            for (int x = 0; x < gridSize - stride; x += stride) {
+                for (int z = 0; z < gridSize - stride; z += stride) {
+                    int a = GetVertexID(x, z, gridSize, stride);
+                    int b = GetVertexID(x + stride, z, gridSize, stride);
+                    int c = GetVertexID(x + stride, z + stride, gridSize, stride);
+                    int d = GetVertexID(x, z + stride, gridSize, stride);
+
+                    mesh.Faces.Add(GenericFace.ReverseWinding(new GenericFace(a, c, b, material, section)));
+                    mesh.Faces.Add(GenericFace.ReverseWinding(new GenericFace(a, d, c, material, section)));
+                }
+            }
+
+            return mesh;
+        }
+
+        private static int GetVertexID(int x, int z, int gridSize, int stride) {
+            return ConvertGridPositionToIndex(new Tuple<int, int>(x / stride, z / stride), gridSize / stride);
+        }
+
+        private static Tuple<int, int> ConvertIndexToGridPosition(int index, int gridSize) {
+            return new Tuple<int, int>(index % gridSize, index / gridSize);
+        }
+
+        private static int ConvertGridPositionToIndex(Tuple<int, int> gridPosition, int gridSize) {
+            return gridPosition.Item2 * gridSize + gridPosition.Item1;
+        }
+
+        private Vector3 ReadVector3Compressed(int offset) {
+            // Inexplicably, position and normal vectors are encoded inside 4 bytes. ~10 bits per component
+            // This seems okay for directions, but positions suffer from stairstepping artifacts
+            uint kBitMask10 = (1 << 10) - 1;
+            uint v = BinaryUtils.ReadUInt32LittleEndian(RawBytes, offset);
+            uint x = (v >> 0) & kBitMask10;
+            uint y = (v >> 10) & kBitMask10;
+            uint z = (v >> 20) & kBitMask10;
+            return new Vector3(x, y, z) / kBitMask10;
+        }
+
+        private static Vector3 ConvertToUE4PositionVector(Vector3 vector) {
+            return new Vector3(vector.Z, vector.X, vector.Y) * 100;
+        }
+
+        private static Vector3 ConvertToUE4PositionVectorTerrain(Vector3 vector) {
+            return new Vector3(vector.X, -vector.Z, vector.Y) * 100;
+        }
+
+        private static Vector3 ConvertToUE4DirectionVector(Vector3 vector) {
+            return new Vector3(vector.Z, vector.X, vector.Y);
         }
     }
 }
