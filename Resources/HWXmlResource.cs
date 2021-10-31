e5f60fad85e7629ac9c7945b92d86e0d250eb521
srogee <joe.sorgea@gmail.com>
2021-10-31 17:52:39 -0500

    use PhxTools to extract xmb files programmatically

--- Resources/HWXmlResource.cs
+++ Resources/HWXmlResource.cs
@@ -1,15 +1,27 @@
-﻿using System.Numerics;
+﻿using System.IO;
+using System.Numerics;
 using System.Xml.Linq;
 
 namespace HaloWarsTools
 {
     public class HWXmlResource : HWResource
     {
-        protected XElement XmlData => ValueCache.Get(() => XElement.Load(AbsolutePath));
+        protected XElement XmlData => ValueCache.Get(Load);
 
         protected static Vector3 DeserializeVector3(string value) {
             string[] components = value.Split(",");
             return new Vector3(float.Parse(components[0]), float.Parse(components[1]), float.Parse(components[2]));
+        }
+
+        XElement Load() {
+            string xmlFile = AbsolutePath;
+            string xmbFile = xmlFile + ".xmb";
+
+            if (!File.Exists(xmlFile)) {
+                KSoft.Phoenix.Resource.ResourceUtils.ConvertXmbToXml(xmlFile, xmbFile, KSoft.Shell.EndianFormat.Big, KSoft.Shell.ProcessorSize.x64);
+            }
+            
+            return XElement.Load(xmlFile);
         }
     }
 }
